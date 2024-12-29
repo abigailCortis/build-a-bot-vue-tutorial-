d0dc66629f7065a5738a6c2cbd942842d3b852b7
Abigail Cortis <cortisbiggirl@gmail.com>
2024-12-29 13:19:01 +0100

    exploring the benefits of the composition api

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,9 @@
 
 <script>
 // import HomePage from './home/HomePage.vue';
+// import RobotBuilder from './build/RobotBuilder.vue';
 import RobotBuilder from './build/RobotBuilder.vue';
+// import ProductSearch from './search/ProductSearch.vue';
 
 export default {
   name: 'App',
